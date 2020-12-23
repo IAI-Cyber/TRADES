f5a2e00326e7c79804d810ddd86d38f0064e1a89
tair <60214721+tairlo@users.noreply.github.com>
2020-12-23 12:54:17 +0200

    External control current work

---
+++ bundles/dsm.TRADES.edit/src-man/dsm/TRADES/provider/ExternalControlItemProviderCustomImpl.java
@@ -0,0 +1,17 @@
+package dsm.TRADES.provider;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+
+public class ExternalControlItemProviderCustomImpl extends ExternalControlItemProvider {
+
+	public ExternalControlItemProviderCustomImpl(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+	
+	@Override
+	public String getText(Object object) {
+		// TODO Auto-generated method stub
+		return super.getText(object);
+	}
+
+}
