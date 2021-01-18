258b122048004e014bf3dd5c731e2326b648222a
tair <60214721+tairlo@users.noreply.github.com>
2021-01-18 17:45:40 +0200

    #Fix bug of importing controls in capec

--- bundles/dsm.TRADES.debug/src/dsm/trades/debug/InitCapec.java
+++ bundles/dsm.TRADES.debug/src/dsm/trades/debug/InitCapec.java
@@ -118,7 +118,7 @@ public class InitCapec implements IApplication {
 		for (int j = 0; j < mitigationsNodes.getLength(); j++) {
 			Element mitigationsNode = (Element) mitigationsNodes.item(j);
 
-			NodeList mitigationNodes = mitigationsNode.getElementsByTagName("CourseOfAction");
+			NodeList mitigationNodes = mitigationsNode.getElementsByTagName("Mitigation");
 			for (int i = 0; i < mitigationNodes.getLength(); i++) {
 				Element mitigatioNode = (Element) mitigationNodes.item(i);
 
