824bbd8c4f04378791237c87a59c55803d6a5182
Arthur Daussy <arthur.daussy@obeo.fr>
2021-05-11 12:03:52 +0200

    Fix broken test

--- bundles/dsm.TRADES.tests/src/dsm/TRADES/impl/ThreatCustomImplTest.java
+++ bundles/dsm.TRADES.tests/src/dsm/TRADES/impl/ThreatCustomImplTest.java
@@ -44,7 +44,7 @@ public class ThreatCustomImplTest extends AbstractTest {
 
 		BasicDiagnostic chain = new BasicDiagnostic();
 		assertThat(threat.checkIsUsed(chain, new HashMap<>())).isFalse(); // Not used
-		assertThat(chain.getChildren()).hasSize(1).allMatch(b -> b.getSeverity() == Diagnostic.WARNING);
+		assertThat(chain.getChildren()).hasSize(1).allMatch(b -> b.getSeverity() == Diagnostic.INFO);
 
 		// Allocated a ThreatMitigationRelation but no component => not used
 
@@ -67,7 +67,7 @@ public class ThreatCustomImplTest extends AbstractTest {
 
 		chain = new BasicDiagnostic();
 		assertThat(threat.checkIsUsed(chain, new HashMap<>())).isFalse(); // Still Not used
-		assertThat(chain.getChildren()).hasSize(1).allMatch(b -> b.getSeverity() == Diagnostic.WARNING);
+		assertThat(chain.getChildren()).hasSize(1).allMatch(b -> b.getSeverity() == Diagnostic.INFO);
 
 		// Allocate the threat
 		thAllocationRel.setThreat(threat);
